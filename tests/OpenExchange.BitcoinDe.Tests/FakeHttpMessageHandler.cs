a6531c03e970e682506d7a6260ce752ed7b3ed43
vogelervo <volkhard.vogeler@it2media.de>
2026-05-17 10:52:24 +0200

    retry new tests-structure

---
+++ tests/OpenExchange.BitcoinDe.Tests/FakeHttpMessageHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace OpenExchange.BitcoinDe.Tests
+{
+    public class FakeHttpMessageHandler : HttpMessageHandler
+    {
+        private readonly string _responseJson;
+
+        public FakeHttpMessageHandler(string responseJson)
+        {
+            _responseJson = responseJson;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(
+                    _responseJson,
+                    Encoding.UTF8,
+                    "application/json")
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
